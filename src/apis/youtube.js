6552b3de3a697104ad3b42a90bceb00e5a5126d7
Rahul Kherajani <rahul.kherajani20@gmail.com>
2020-07-12 20:00:47 +0530

    Hiding API Keys

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyDvMXNryA2AuLlTOA8Sh8ZlCLPp-nNSLPs";
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
